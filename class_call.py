254e8fb4c3b8bc773fe80e50f5bdffc2c21c4fd5
Debjoy <sahadebjoy10@gmail.com>
2019-08-31 20:51:58 +0530

    Improved FPS

--- class_call.py
+++ class_call.py
@@ -2,6 +2,8 @@ from djitellopy import Tello
 import cv2
 from class_FtextR_final import warehouse_R
 from class_FtextL_final import warehouse_L
+from csvParserfinal import *
+from tello_height import *
 
 class warehouse_overall:
 	def __init__(self, tello):
@@ -11,9 +13,16 @@ class warehouse_overall:
 
 	def algo(self):
 		
-		self.txt_L.scan()
+		self.txt_R.scan()
+		h = self.tello.get_h()
 		# GO_down_1.5m
+		goto_height(self.tello, h - 150)
 		
-		#self.txt_L.scan()
+		self.txt_L.scan()
 		# GO_up_1.5m
+
+		parser1()
+		parser2()
+
+
 		
